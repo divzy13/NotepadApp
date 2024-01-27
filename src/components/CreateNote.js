e2a693bf5d30f99f00f7c64250abb1b6eb196bce
DIRAV1 <divya.raveendran1@inter.ikea.com>
2024-01-27 18:38:23 +0100

    linearprogress

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -1,6 +1,9 @@
 import React from "react";
+import LinearProgress from "@mui/material/LinearProgress";
 
 function CreateNote({textHandler, saveHandler, text}) {
+  const charLimit = 100;
+  const charLeft = charLimit - text.length;
   return (
     <div className="note" style={{ background: "rgba(255, 255, 255, 0)" }}>
       <textarea
@@ -12,9 +15,14 @@ function CreateNote({textHandler, saveHandler, text}) {
         onChange={textHandler}
       ></textarea>
       <div className="note__footer">
-        <span className="label"> left</span>
+        <span className="label"> {charLeft} left</span>
         <button className="note__save" onClick={saveHandler}>Save</button>
       </div>
+      <LinearProgress
+        className="char__progress"
+        variant="determinate"
+        value={charLeft}
+      />
     </div>
   );
 }
